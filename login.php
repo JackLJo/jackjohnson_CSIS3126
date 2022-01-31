07872ca75cbf65d7aa13837cf3d6d7057ca13f15
Jack <j02179817@jwu.edu>
2022-01-31 13:17:36 -0500

    First commit. Unworking register page, to be fixed.

---
+++ login.php
@@ -0,0 +1,16 @@
+<!DOCTYPE HTML>
+
+<html>
+<head>
+  <title>Log in</title>
+</head>
+<body>
+  <form action="login_process.php" method = "post">
+
+  Email : <input type="text" name="email"><br />
+  Password : <input type="password" name="pw"><br />
+
+  <input type="submit" value="Log in">
+
+</body>
+</html>
