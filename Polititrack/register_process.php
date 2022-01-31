c77325f70983dc5d45b3c6b451a7a2e1667a17af
Jack <j02179817@jwu.edu>
2022-01-31 15:05:08 -0500

    Added error control for register

--- Polititrack/register_process.php
+++ Polititrack/register_process.php
@@ -1,9 +1,31 @@
 <!DOCTYPE HTML>
 
 <html>
+
+  <h3>
+  <?php
+
+  $errormessage .= ($_POST["email"] == "" ? "You must enter an email. <br/>":"");
+  $errormessage .= (strlen($_POST["pw"]) <= 7 ? "You must enter a password with atleast 7 characters <br/>":"");
+  $errormessage .= (filter_var($_POST["email"], FILTER_VALIDATE_EMAIL) == false?"You must enter a valid email address.<br/>":"");
+
+  if(strlen($errormessage) > 0){
+
+    echo $errormessage;
+  ?>
+  </h3>
+
 <body>
 
   <?php
+    include("register.php");
+    die();
+  }
+  ?>
+
+
+  <?php
+
   $connection = mysqli_connect("localhost","root","root","polititrack");
   session_start();
 
@@ -11,7 +33,7 @@
   $email = mysqli_real_escape_string($connection, $_POST["email"]);
   $password = md5(mysqli_real_escape_string($connection, $_POST["pw"]));
 
-  $res = mysqli_query($connection, "select * from users where email = '$email'");
+  $res = mysqli_query($connection, "select * from users where email = \"".$email."\"");
   $row = mysqli_fetch_assoc($res);
 
   if($row){
@@ -19,7 +41,9 @@
     die();
   }
   else{
-    $res = mysqli_query($connection, "insert into users (email, password) values ('$email' '$pw')");
+    $res = mysqli_query($connection, "insert into users (email, password) values ('$email', '$password')");
+
+    echo "Successfully registered user! <br /><br />Please <a href=\"login.php\">Log in!</a>";
   }
 
   mysqli_close($connection);
