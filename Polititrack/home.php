589680bbba2a4569e64607000074fb9d6ffaba32
Jack <j02179817@jwu.edu>
2022-02-01 15:47:10 -0500

    Testing with cURL, Google Civic Info API

---
+++ Polititrack/home.php
@@ -0,0 +1,21 @@
+<!DOCTYPE HTML>
+
+<html>
+<h1>
+  Welcome!
+</h1>
+<h2>
+  Please select an option below.
+</h2>
+
+<body>
+<br /><br />
+
+<a href="profile.php">Your profile</a><br/>
+
+
+
+
+</body>
+
+</html>
